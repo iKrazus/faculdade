f1e00044720c10159dd5ce189d3ddf59aaf81059
iKrazus <73796298+iKrazus@users.noreply.github.com>
2020-11-01 16:39:32 -0300

    Create Criptografia.py

---
+++ Aps/Criptografia.py
@@ -0,0 +1,30 @@
+def crip():
+    palavra = input("Digite uma palavra para criptografar: ")
+    cripto=' '
+    for i in range(0,len(palavra)):
+        cripto = cripto+chr(ord(palavra[i])+10)
+    print(cripto)
+
+    
+
+def descrip():
+    palavra = input("Digite uma palavra para criptografar: ")
+    descripto=' '
+    for i in range(0,len(palavra)):
+        descripto = descripto+chr(ord(palavra[i])-10)
+    print(descripto)
+
+     
+
+op = 'N'
+while op != 3:
+    print("\t\t\t Criptografia \n")
+    print("Digite '1' para Criptografar")
+    print("Digite '2' Descriptografar")
+    print("Digite '3' para sair do sistema")
+    op = int(input("\n Digite sua opção ou '3' para sair: "))
+
+    if op == 1:
+        crip()
+    if op == 2:
+        descrip()
